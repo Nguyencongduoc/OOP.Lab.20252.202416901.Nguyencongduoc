b736fa48f18449425d54eb4ff7ba7e1d5c4370ba
Abcxyzcmnl2006 <baconsoixam06hp@gmail.com>
2026-04-14 15:41:38 +0700

    Update

---
+++ OtherProjects/Lab01/FirstDialog.java
@@ -0,0 +1,7 @@
+import javax.swing.JOptionPane;
+public class FirstDialog{
+    static void main(String args[]){
+        JOptionPane.showMessageDialog(null, "Hello World! \n How are you?");
+        System.exit(0);
+    }
+}
